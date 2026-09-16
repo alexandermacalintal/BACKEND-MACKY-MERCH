be5d06a9485833d48c142c220c2a31ba2b24021f
alexandermacalintal <alexander_macalital@dlsu.edu.ph>
2026-09-16 13:18:08 +0800

    Version 0.8

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ import productRouter from './routes/products.routes.js';
 
 import db from './db.js';
 
-import errorMiddleware from './middlewares/error.middleware.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/products', productRouter);
-app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send({title: 'Macky Merch API', note: 'Welcome to the Macky Merch API!'});
 });
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
